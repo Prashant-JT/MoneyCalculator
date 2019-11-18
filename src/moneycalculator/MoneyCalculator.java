b93b48812f83a307be316da9c3b2984b2b78bbb8
Prashant-JT <prash@DESKTOP-3D4J7JB>
2019-11-18 20:23:22 +0000

    Versión 2.2 de la práctica MoneyCalculator (CIPO)

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -8,53 +8,47 @@ import java.net.URLConnection;
 import java.util.Scanner;
 
 public class MoneyCalculator {
-    double amount;
-    double exchangerate;
-    String currencyFrom;
-    String currencyTo;
+    private double amount;
+    private double exchangeRate;
+    private String currencyFrom;
+    private String currencyTo;
     
     public static void main(String[] args) throws IOException {
-        MoneyCalculator moneycalculator = new MoneyCalculator();
-        moneycalculator.control();
+        MoneyCalculator moneyCalculator = new MoneyCalculator();
+        moneyCalculator.control();
     }
-
+    
     private void control() throws IOException {
         input();
         process();
         output();
     }
-
-    private void input() {
-        System.out.println("Introduce una cantidad: ");
+    
+    private void input(){
         Scanner scanner = new Scanner(System.in);
-        amount = scanner.nextDouble();
-        
-        System.out.println("Introduce una divisa inicial: ");
+        System.out.println("Introduce una cantidad: ");
+        amount = Double.parseDouble(scanner.next());
+        System.out.println("Introduce una divisa origen: ");
         currencyFrom = scanner.next();
-        
-        System.out.println("Introduce una divisa final: ");
+        System.out.println("Introduce una divisa destino: ");
         currencyTo = scanner.next();
     }
-
-    private void process() throws IOException {
-        exchangerate = getExchangeRate(currencyFrom,currencyTo);
+    
+    private void process() throws IOException{
+        this.exchangeRate = getExchangeRate(currencyFrom, currencyTo);
     }
-
-    private void output() {
-        System.out.println(amount + " " + currencyFrom + " = " +
-                amount*exchangerate + currencyTo);
+    
+    private void output(){
+        System.out.println(this.amount + " " + currencyFrom + " equivalen a: " + amount*exchangeRate + " " + currencyTo);
     }
-
-    private static double getExchangeRate(String from, String to) throws IOException {
-        URL url = 
-            new URL("http://free.currencyconverterapi.com/api/v5/convert?q=" +
-                    from + "_" + to + "&compact=y");
+    
+    private static double getExchangeRate(String from, String to) throws IOException{
+        URL url = new URL("http://free.currencyconverterapi.com/api/v5/convert?q=" + from + "_" + to + "&compact=y&apiKey=1eb50c98f4a50de7d844");
+        MoneyCalculator moneyCalculator = new MoneyCalculator();
         URLConnection connection = url.openConnection();
-        try (BufferedReader reader = 
-                new BufferedReader(
-                        new InputStreamReader(connection.getInputStream()))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
             String line = reader.readLine();
-            String line1 = line.substring(line.indexOf(to)+12, line.indexOf("}"));
+            String line1 = line.substring(line.indexOf(to) + 12, line.indexOf("}"));
             return Double.parseDouble(line1);
         }
     }
